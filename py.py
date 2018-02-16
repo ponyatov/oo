a5c870b0c8a60873abd3915e9fd01abfea08d4ee
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-02-16 14:38:09 +0400

    + core Object/type
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- py.py
+++ py.py
@@ -1,2 +1,10 @@
 # object/stack virtual machine
 
+class Object:
+    def __init__(self,V):
+        self.type = self.__class__.__name__.lower()
+        self.value = V
+    def __repr__(self):
+        return '<%s:%s>'%(self.type,self.value)
+
+print Object('test')
