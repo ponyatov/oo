1a113b2eac6760b56520f402456629f498bbc6d9
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-03-14 18:02:07 +0400

    + label
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- micro/micro.c
+++ micro/micro.c
@@ -11,16 +11,33 @@ UCELL Ip=0;
 
 void _NOP() { printf("nop"); }
 void _BYE() { printf("bye\n\n"); exit(0); }
+
+								/* control flow */
+
 void _JMP() { Ip = get(Ip); printf("jmp\t%.4X",Ip); assert(Ip<Cp); }
+
+								/* debug */
+
+void logstr(UCELL addr) {
+	BYTE len = M[addr];
+	UCELL i; for (i=addr+1;i<=addr+len;i++) printf("%c",M[i]);
+}
+
+void _LABEL() {
+	UCELL addr = get(Ip); Ip += 2;
+	printf("label\t%.4X:",addr); logstr(addr); }
+
+
 
 void VM() {						/* virtual machine startup */
 	for (;;) {
 		BYTE op = M[Ip++];
 		printf("\n%.4X:%.2X\t",Ip-1,op);
 		switch (op) {
-			case NOP: _NOP(); break;
-			case BYE: _BYE(); break;
-			case JMP: _JMP(); break;
+			case NOP:		_NOP();		break;
+			case BYE:		_BYE();		break;
+			case JMP:		_JMP();		break;
+			case LABEL:		_LABEL();	break;
 			default: abort();
 		}
 	}
